4c3ead99cf11dfeec1707100df3ade2aa48a4156
Don Drake <ddrake@dreamingmind.com>
2015-07-21 12:04:38 -0700

    update samples page

--- src/Controller/SamplesController.php
+++ src/Controller/SamplesController.php
@@ -86,6 +86,7 @@ foreach ($samples as $sample) {
 		'<?php
 if (!$this->Geshi->templates(\'Imp\')) {
 	$this->Geshi->template(\'Imp\', \'php\')->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS);
+	$this->Geshi->template(\'Imp\')->set_overall_style(\'background: #eefcfc;\');
 	$this->Geshi->template(\'Imp\')->set_line_style(\'background: #eefcfc;\');
 }
 ?>
